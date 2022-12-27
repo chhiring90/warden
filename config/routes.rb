80d32c4abee3dce69256ec14d215ee91cbe77c85
chhiring <chhiring@supremeitsolutions.com>
2022-12-27 15:07:21 +0545

    modify: auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'auth', format: false, controllers: {
+    sessions: 'auth/sessions',
+    # registrations: 'auth/registrations'
+  }
+
+  as :user do
+    post 'login', to: 'devise/sessions#new'
+    post 'signup', to: 'auth/registrations#create'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
